c3a68c54d27bab0b9ea1d6d049d68ce3e8e00a8a
Iza Rogoś <izabela.rogos@gmail.com>
2017-02-21 23:30:51 +0100

    Extracted class for working with groups. Refactor - name of fixture/group.py correction.

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
 
 
 class Application:
@@ -9,6 +10,7 @@ class Application:
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
         wd = self.wd
@@ -16,34 +18,6 @@ class Application:
 
     def destroy(self):
         self.wd.quit()
-
-    # additional methods - adding group
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("grupy").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_groups_page()
-        # Init group creation
-        wd.find_element_by_name("new").click()
-        # Fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # Submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
-
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
 
     # additional methods -adding contact
     def submit_contact(self):
